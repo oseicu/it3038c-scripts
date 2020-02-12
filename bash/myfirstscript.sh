e6c1767527bf401da9d0aa9db3e6aa556e0b9d74
oseicu <oseicu@mail.uc.edu>
2020-02-11 20:08:23 -0500

    bash

---
+++ bash/myfirstscript.sh
@@ -0,0 +1,10 @@
+"shabang"
+#!/bin/bash
+# This scripts outputs the IP address and Hostname of a machine
+echo 'This is a script. Hello!'
+greeting="This is a acript. Hello!"
+echo $greeting, thanks for joining us!
+echo '$greeting, thanks for joining us ! You owe me $20'
+echo "$greeting, thanks for joining us!"
+echo "greeting, you owe me \$20"
+

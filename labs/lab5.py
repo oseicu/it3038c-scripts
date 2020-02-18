1a5667983c96e67436877b0008664f6be1c9529d
oseicu <oseicu@mail.uc.edu>
2020-02-17 19:06:42 -0500

    mmm

--- labs/lab5.py
+++ labs/lab5.py
@@ -1,1 +1,15 @@
-hostname
+import datetime
+
+Year = int(input("Please enter the year you were born"))
+Month = int(input("Please enter the number of the month you were born. 3 = March"))
+Day = int(input("Please enter the day you were born"))
+
+DOB = datetime.ddatetime(Year,Month,Day)
+Age = (datetime.datetime.now()-DOB)
+
+print("You are" + str(Age.days) + "days old")
+
+convertdays = int(Age.days)
+AgeYears =Convertdays/365
+
+pront("Or" + str(AgeYears) + " years old to be less precise!")

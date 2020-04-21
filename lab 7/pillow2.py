90d59600ac48bf0f063e63e8e3e9b6074a8eb933
oseicu <oseicu@mail.uc.edu>
2020-04-21 19:03:54 -0400

    lab 8a

--- lab 7/pillow2.py
+++ lab 7/pillow2.py
@@ -1,16 +1,17 @@
 import os
+
 from PIL import Image
 
-image1 = Image.open('pixel.jpg'):
-image1.convert(mode='L').save('pixel_mod.jpg')
+
+image2 = Image.open('pixel.jpg'):
+image2.convert(mode='L').save('pixel_mod.jpg')
 
 size_500 = (500,500)
- for f in os.listdir('.'):
-   if f.endswitch('.jpg'):
-     i = Image.open(f)
-     fn, fext = os.path.splitext(f)
-     i.thumbnail(size_500)
-     i.save('500/{}_500()'.format(fn, fext))
-   
-   
-     print fext
+for f in os.listdir('.'):
+  if f.endswitch('.jpg'):
+    i = Image.open(f)
+    fn, fext = os.path.splitext(f)
+    i.thumbnail(size_500)
+    i.save('500/{}_500()'.format(fn, fext))
+  
+    print fext

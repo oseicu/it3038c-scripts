f21e8cfa01c640f84a3a2d7437705adf64e0a06d
oseicu <oseicu@mail.uc.edu>
2020-04-23 09:57:02 -0400

    Lab 9C

---
+++ labC/weatherapi.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+print('Please enter your zip code: ')
+zip = input()
+
+r = requests.get('https://localhost:3000' % zip)
+#print(type(r.content))
+data = r.json()
+
+widgets = "widget1=blue widget2=green widget3=black widgetX=blue"
+data = widgets.split()  #split string into a list
+
+for temp in data:
+    print temp
+    
+#print(type(data))
+print(data['weather'][0]['description'])
+
+
+
---
+++ nodes/api.js
@@ -0,0 +1,29 @@
+var http = require("http");
+var data = require("C:/Windows/Temp/widgets.json");
+
+var server = http.createServer(function(req, res){
+    if (req.url == "/"){
+        res.writeHead(200, {"Content-Type": "text/json"});
+        res.end(JSON.stringify(data));
+    }
+    else if (req.url === "/blue"){
+        listBlue(res);
+    }
+else {
+          res.writeHead(404, {"Content-Type": "text/plain"});
+          res.end("Data not found");
+      }
+});
+
+
+function listBlue(res){
+    var colorBlue = data.filter(function(item){
+        return item.color === "blue";
+});
+
+    res.end(JSON.stringify(colorBlue));
+}
+
+server.listen(3000);
+console.log("Server is listening on port 30000");
+

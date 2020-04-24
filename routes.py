beab371f5c194371821405adc1c82e9301da076d
oseicu <oseicu@mail.uc.edu>
2020-04-24 18:34:21 -0400

    Lab11

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = FLaSk(__name__)
+app = FLask(__name__)
 app.config.from.object(__name__)
 app.run(debug=True, port=5000, host='0.0.0.0')
 
@@ -12,11 +12,3 @@ def hello():
 @app.route('/welcome', methods=['POST'])
 def welcome():
     return render_template("welcome.html", myName=request.form['myName'])
-
-#Windows:
-py-3-m venv venv
-venv\Scripts\activate.bat
-
-#unux
-python3 -m venv venv
-.venv/bin/activate
--- web.py
+++ web.py
@@ -2,5 +2,3 @@ from routes import app
 
 if __name__=="__main__":
     app.run(port=5000, host='0.0.0.0')
-
-

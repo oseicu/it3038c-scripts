477357c82fc31f5b184c7c8852e1ebfd332101fe
oseicu <oseicu@mail.uc.edu>
2020-04-27 11:46:49 -0400

    Pro 3

--- Project 3/dates.js
+++ Project 3/dates.js
@@ -9,9 +9,39 @@ diff = firstDate - secondDate;
 firstDate.getTime();
 secondDate.getTime();
 
+//display dates in days, hours, seconds, minute, year
+var firstDate = "Feb. 2, 2018";
+var secondDate = "Apr. 4, 2005"
+
+var secondsBetweenTHenAnd1980 = Date.parse(Date);
+var secondsBetweenNowAnd1980 = Date.now(Date);
+var dateInseconds = secondsBetweenNowAnd2000-secondsBetweenThenAnd2000;
+
+var second = 1000;
+var minute = second*60;
+var hour = minute*60;
+var day = hour*24;
+var month = day*30;
+var year = month*365;
+
+//let date conversion begin
+var years = Math.round(seconds/year);
+var months = years*12;
+var days = years*365;
+var hours = Math.round(seconds/hour);
+
+function printResults(){
+    var message = "Date in Years"
+    "</br>Date in Months : "+months+
+    "</br>Date in Days : "+days+
+    "</br>Date in Hours : "+hours+
+    "</br>Date in Seconds : "+seconds
+return message
+}
+
 console.log(Math.floor(diff / days) + " days have passed since the start of the year. ");
 
-var http = require("http");
+
 var server = http.createServer(function(req, res){
     if(req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function(err, body){
@@ -33,12 +63,7 @@ var server = http.createServer(function(req, res){
         <title>Node JS Response</title>
     </head>
     <body>
-      <p>Hostname: ${myHostName}</p>
-      <p>IP: ${ip.address()}</p>
-      <p>Server Uptime: ${myServerUptime}</p>
-      <p>Total Memory: ${myTotalMem}</p>
-      <p>Free Memory: ${myFreeMem}</p>
-      <p>Number of CPUs: ${myCPU}</p>
+      <p>Hostname: ${printResults()}</p>
     </body>
     </html>`
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -49,6 +74,6 @@ else {
     res.end(`404 File Not Found at ${req.url}`);
   }
 })
-server.listen(3000);
+server.listen(3001);
 
 console.log("Server listening on port 3000");
